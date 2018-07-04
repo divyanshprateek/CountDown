e22f0f590b44db5f7cdaa6318536a68596f82321
divyansh <divyanshprateek167dp93@gmail.com>
2018-07-04 03:11:40 -0700

    Created a times table using seekbar

--- app/src/main/java/com/example/divyansh/linearlayout/MainActivity.java
+++ app/src/main/java/com/example/divyansh/linearlayout/MainActivity.java
@@ -8,35 +8,64 @@ import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    public void createTable(int times)
+    {
+        final ArrayList<String> table  = new ArrayList<String>();
 
+        for(int i = 1; i <= 10;i++)
+        {
+            table.add(times+ " * " + i + " = "+ Integer.toString(times * i));
+        }
+
+        ArrayAdapter<String> arrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,table);
+        list.setAdapter(arrayadapter);
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView list = (ListView) findViewById(R.id.mylist);
+        list = (ListView) findViewById(R.id.list);
+        final SeekBar seek = (SeekBar) findViewById(R.id.seek);
+        seek.setMax(20);
+        seek.setProgress(10);
 
-        final ArrayList<String> arraylist  = new ArrayList<String>();
-        arraylist.add("Divyansh");
-        arraylist.add("Bhavya");
-        arraylist.add("Abhijit");
-        arraylist.add("Raunak");
-        arraylist.add("Karan");
-
-        ArrayAdapter<String> arrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,arraylist);
-        list.setAdapter(arrayadapter);
-
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(),"Hello " + arraylist.get(i),Toast.LENGTH_LONG).show();
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int min = 1;
+                int times;
+                if(i < min)
+                {
+                    times = min;
+                    seek.setProgress(min);
+                }
+                else
+                {
+                    times = i;
+                }
+                createTable(times);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
-
+        createTable(10);
     }
+
 }
